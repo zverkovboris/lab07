746deec361b1ca042206b124636ddf7e020fb3cc
zverkovboris <orient-bz@mail.ru>
2018-05-29 20:36:03 +0300

    added documentation

--- README.md
+++
@@ -1,2 +0,0 @@
-[![Build Status](https://travis-ci.org/zverkovboris/lab07.svg?branch=master)](https://travis-ci.org/zverkovboris/lab07)
-Files added
--- include/print.hpp
+++
@@ -1,7 +0,0 @@
-// use command edit
-#include <string>
-#include <fstream>
-#include <iostream>
-
-void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
